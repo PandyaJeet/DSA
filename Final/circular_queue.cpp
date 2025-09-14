b0e514bb244141f17bbba63ceb5f0a5e9c04b30b
Pandya Jeet <jeetpandya2006@gmail.com>
2025-09-14 18:53:08 +0530

    circular queue

---
+++ Final/circular_queue.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+using namespace std;
+#define MAX 5
+
+int arr[MAX], ele, FRONT = -1, REAR = -1;
+
+void insert() {
+    if ((REAR == MAX - 1 && FRONT == 0) || (FRONT ==REAR + 1 )) {
+        cout << "Queue is full\n";
+        return;
+    }
+    cout << "Enter element: ";
+    cin >> ele;
+    if (FRONT == -1) {
+        FRONT = REAR = 0;
+    } else {
+        if (REAR == MAX - 1)
+            REAR = 0;
+        else
+            REAR++;
+    }
+    arr[REAR] = ele;
+}
+
+void delet() {
+    if (FRONT == -1) {
+        cout << "Queue is empty\n";
+        return;
+    }
+    cout << "Element removed: " << arr[FRONT] << endl;
+    if (FRONT == REAR) {
+        FRONT = REAR = -1;
+    } else {
+        if (FRONT == MAX - 1)
+            FRONT = 0;
+        else
+            FRONT++;
+    }
+}
+
+void display() {
+    if (FRONT == -1) {
+        cout << "Queue is empty\n";
+        return;
+    }
+    cout << "Queue elements:\n";
+    int i = FRONT;
+    while (true) {
+        cout << i << ". " << arr[i] << endl;
+        if (i == REAR) break;
+        i = (i + 1) % MAX;
+    }
+}
+
+int main() {
+    int ch;
+    while (true) {
+        cout << "\n1. Insert\n2. Delete\n3. Display\n4. Exit\nChoice: ";
+        cin >> ch;
+        switch (ch) {
+            case 1:
+                insert();
+                break;
+            case 2:
+                delet();
+                break;
+            case 3:
+                display();
+                break;
+            case 4:
+                return 0;
+            default:
+                cout << "Invalid choice\n";
+        }
+    }
+}
+
---
+++ Final/ideal_circular_queue.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+using namespace std;
+#define MAX 5
+
+int arr[MAX], ele, FRONT = -1, REAR = -1;
+
+void insert() {
+    if ((FRONT == 0 && REAR == MAX - 1) || (REAR + 1) % MAX == FRONT) {
+        cout << "Queue is full\n";
+        return;
+    }
+    cout << "Enter element: ";
+    cin >> ele;
+    if (FRONT == -1) {
+        FRONT = REAR = 0;
+    } else {
+        REAR = (REAR + 1) % MAX;
+    }
+    arr[REAR] = ele;
+}
+
+void delet() {
+    if (FRONT == -1) {
+        cout << "Queue is empty\n";
+        return;
+    }
+    cout << "Element removed: " << arr[FRONT] << endl;
+    if (FRONT == REAR) {
+        FRONT = REAR = -1;
+    } else {
+        FRONT = (FRONT + 1) % MAX;
+    }
+}
+
+void display() {
+    if (FRONT == -1) {
+        cout << "Queue is empty\n";
+        return;
+    }
+    cout << "Queue elements:\n";
+    int i = FRONT;
+    while (true) {
+        cout << i << ". " << arr[i] << endl;
+        if (i == REAR) break;
+        i = (i + 1) % MAX;
+    }
+}
+
+int main() {
+    int ch;
+    while (true) {
+        cout << "\n1. Insert\n2. Delete\n3. Display\n4. Exit\nChoice: ";
+        cin >> ch;
+        switch (ch) {
+            case 1:
+                insert();
+                break;
+            case 2:
+                delet();
+                break;
+            case 3:
+                display();
+                break;
+            case 4:
+                return 0;
+            default:
+                cout << "Invalid choice\n";
+        }
+    }
+}
