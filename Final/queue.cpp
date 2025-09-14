f9a5f8e1377d53a2c460fc43a7c02ee5707944b0
Pandya Jeet <jeetpandya2006@gmail.com>
2025-09-15 04:33:31 +0530

    formatting

--- Final/queue.cpp
+++ Final/queue.cpp
@@ -1,54 +1,65 @@
 #include <iostream>
 using namespace std;
 #define MAX 5
-int arr[MAX],ele,FRONT = -1 , REAR = -1;
-void insert (){
-    if(REAR == MAX-1){
-        cout<< "Queue is full \n";
+int arr[MAX], ele, FRONT = -1, REAR = -1;
+void insert()
+{
+    if (REAR == MAX - 1)
+    {
+        cout << "Queue is full \n";
         return;
     }
     cout << "Enter element : ";
     cin >> ele;
     arr[++REAR] = ele;
-    if(FRONT == -1 ) FRONT++;
+    if (FRONT == -1)
+        FRONT++;
 }
-void delet(){
-    if(FRONT == -1){
+void delet()
+{
+    if (FRONT == -1)
+    {
         cout << "Queue is empty\n";
         return;
     }
-    cout << "Element removed " << arr[FRONT]<<endl;
-    if(FRONT == REAR ) FRONT = REAR = -1;
-    else FRONT++;
+    cout << "Element removed " << arr[FRONT] << endl;
+    if (FRONT == REAR)
+        FRONT = REAR = -1;
+    else
+        FRONT++;
 }
-void display(){
-    if(FRONT==-1 && REAR ==-1){
+void display()
+{
+    if (FRONT == -1 && REAR == -1)
+    {
         cout << "Queue is empty \n";
         return;
     }
-    for(int i = FRONT ; i <= REAR ; i ++ ){
-        cout << i <<". " << arr[i] << endl ;
+    for (int i = FRONT; i <= REAR; i++)
+    {
+        cout << i << ". " << arr[i] << endl;
     }
 }
-int main(){
+int main()
+{
     int ch;
     cout << "Insert\nDelete\nDisplay\nChoice : ";
     cin >> ch;
-    switch (ch){
-        case 1:
-            insert();
-            main();
-            break;
-        case 2:
-            delet();
-            main();
-            break;
-        case 3:
-            display();
-            main();
-            break;
-        default:
-            return 0;
+    switch (ch)
+    {
+    case 1:
+        insert();
+        main();
+        break;
+    case 2:
+        delet();
+        main();
+        break;
+    case 3:
+        display();
+        main();
+        break;
+    default:
+        return 0;
     }
 }
-
